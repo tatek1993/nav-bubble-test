511358a4ca07abbcab2b4f6020b3851bf482b8c4
Tate Keller <tatek1993@gmail.com>
2020-04-23 20:52:57 -0500

    made animated navigation bar test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Link } from 'react-router-dom';
+
+const menuItems = [
+  'Home', 
+  'Support',
+  'Messages',
+  'Browse'
+]
+
+const dropImageAspectRatio = 190.49475/59.402695;
 
 function App() {
+  const [dropPosition, setDropPosition] = useState({left: 0, top: 0});
+ 
+  const [hoveredItem, setHoveredItem] = useState(null);
+  useEffect(() => {
+      let dropRect = document.getElementById('drop').getBoundingClientRect();
+      let navRect = document.querySelector('nav').getBoundingClientRect();
+    if(hoveredItem) {
+      let linkRect = document.getElementById(hoveredItem).getBoundingClientRect();
+      
+      setDropPosition({
+        left: ((linkRect.left+linkRect.right)-dropRect.width)/2,
+        top: navRect.bottom-4
+      });
+    } else {
+      setDropPosition({
+        left: dropPosition.left,
+        top: (navRect.bottom-(dropRect.width/dropImageAspectRatio)/3)-4
+      })
+    }
+  
+  }, [hoveredItem])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="nav">
+        <nav>
+          <h1 className='nav-header'>Canary</h1>
+          <img 
+            id='drop' 
+            style={{left: `${dropPosition.left}px`, top: `${dropPosition.top}px`}} 
+            className={hoveredItem && 'active'}
+            src="../menudrop.svg"
+          />
+          {
+            menuItems.map(item => (
+            <Link 
+              className='nav-link' 
+              key={item} 
+              id={item}
+              onMouseEnter={ () => setHoveredItem(item) }
+              onMouseLeave={ () => setHoveredItem(null) }
+              >
+                {item}
+            </Link>
+            ))
+          }
+        </nav>     
       </header>
+
     </div>
   );
 }
